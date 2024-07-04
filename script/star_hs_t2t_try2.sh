77105cc0e4d89b25a2b7325bf982f98f15109784
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-07-04 12:42:04 +0200

    Add files via upload

---
+++ script/star_hs_t2t_try2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for run in $(ls /home/aazzolini/gdebiase/fastp); do 
+    for seq in $(ls /home/aazzolini/gdebiase/fastp/${run} | grep _R1_ | cut -d R -f 1 | uniq); do
+        conda run -n al STAR \
+        --runThreadN 8 \
+        --genomeDir /home/aazzolini/gdebiase/hs_T2T/hs_T2T_genomeindex \
+        --readFilesCommand zcat \
+        --readFilesIn /home/aazzolini/gdebiase/fastp/${run}/${seq}R1_trimmed.fastq.gz /home/aazzolini/gdebiase/fastp/${run}/${seq}R2_trimmed.fastq.gz \
+        --alignEndsType EndToEnd \
+        --outSAMattributes Standard \
+        --outFilterMismatchNoverReadLmax 0.04 \
+        --outReadsUnmapped Fastx \
+        --outSAMprimaryFlag AllBestScore \
+        --outFilterType BySJout \
+        --outSAMtype BAM SortedByCoordinate \
+        --chimOutType WithinBAM HardClip \
+        --alignSJoverhangMin 6 \
+        --alignSJDBoverhangMin 6 \
+        --clip5pNbases 5 \
+        --clip5pNbases 5 \
+        --outFileNamePrefix /home/aazzolini/gdebiase/hs_T2T/${run}_STAR_mapping_result/${seq}
+    done
+done
